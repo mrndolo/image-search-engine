24ea81e151e01d228801c2919bd46cdb7e483850
mrndolo <callistomutunga@gmail.com>
2024-07-30 20:32:00 +0300

    selected the form elements with JS

---
+++ script.js
@@ -0,0 +1,10 @@
+let accessKey = "";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
