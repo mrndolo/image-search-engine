98d927ef357f52fd03a42ff3f2b3aac7e4803640
mrndolo <callistomutunga@gmail.com>
2024-07-30 20:32:31 +0300

    Added code to fetch api data

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const showMoreBtn = document.getElementById("show-more-btn");
 let keyword = "";
 let page = 1;
 
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = "";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // console.log(data);
+
+    const results = data.results;
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
